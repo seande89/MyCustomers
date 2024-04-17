454b4fefd6b4bdaa65a2f3bda98de2610c4a66e4
Sean <razasoftware.tt@gmail.com>
2024-04-16 22:27:23 -0400

    Validation phase 2

--- MyCustomers/Models/Vehicle.cs
+++ MyCustomers/Models/Vehicle.cs
@@ -13,15 +13,21 @@ namespace MyCustomers.Models
         public int CustomerID { get; set; }
         [Required]
         public int VehicleMakeID { get; set; }
+
         [Required(ErrorMessage = "Please enter the model of the vehicle")]
         public string VehicleModel { get; set; }
+
         [Required(ErrorMessage = "Please enter a year")]
-        [Range(1920, 2124, ErrorMessage = "Year must be after 1920")]
+        [Range(1920, 2124, ErrorMessage = "Year must be between 1920 and 2124")]
         public int VehicleYear { get; set; }
+
         [Required(ErrorMessage = "Please enter the VIN or chassis number")]
+        [RegularExpression("^[A-Z0-9]{17}$", ErrorMessage = "Invalid Vehicle Identification Number Format.")]
         public string VehicleIDNum { get; set; }
+
         [Required]
         public DateTime DateCreated { get; set; }
+
         [Required(ErrorMessage = "Please enter the license plate number")]
         public string VehiclePlateNum { get; set; }
 
